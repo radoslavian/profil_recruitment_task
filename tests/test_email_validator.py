a07acadc540dc86835fa533107c04018e712cf0d
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-27 21:28:25 +0100

    Wrote email validator.

--- tests/test_email_validator.py
+++ tests/test_email_validator.py
@@ -1,16 +1,130 @@
 import unittest
 from utils.email_validator import validate_email
+from utils.exceptions import InvalidEmailError
 
 
-class EmailValidatorTestCase(unittest.TestCase):
+class EmailValidatorAtCharacterTestCase(unittest.TestCase):
     """
     Email must contain only one "@" symbol.
     """
     def test_single_at(self):
         """
-        Single '@' in an email address: should return True.
+        Single '@' in a valid email address: should return True.
         """
-        self.assertEqual(True, False)  # add assertion here
+        valid_email = "user@domain.com"
+        result = validate_email(valid_email)
+        self.assertTrue(result)
+
+    def test_multiple_at(self):
+        """
+        Multiple '@' put in different places of an email address:
+        should raise InvalidEmailError.
+        """
+        invalid_email = "user1@very@long@domain@name.com"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(invalid_email))
+
+    def test_multiple_at_next_to_each_other(self):
+        """
+        Multiple '@' put next to each other in an email address:
+        should raise InvalidEmailError.
+        """
+        invalid_email = "user@@domain.com"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(invalid_email))
+
+    def test_no_at(self):
+        """
+        No '@' character in an email address:
+        should raise InvalidEmailError.
+        """
+        invalid_email = "user_at_domain.com"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(invalid_email))
+
+
+class EmailValidatorUsernameTestCase(unittest.TestCase):
+    """
+    The part before "@" (email username) must be at least 1 character long.
+    """
+    def test_single_character(self):
+        """
+        Should success: single character for an email username.
+        """
+        email_address = "a@domain.com"
+        result = validate_email(email_address)
+        self.assertTrue(result)
+
+    def test_multiple_characters(self):
+        """
+        Should success: multiple characters for an email username.
+        """
+        email_address = "username_1@domain.com"
+        result = validate_email(email_address)
+        self.assertTrue(result)
+
+    def test_no_username(self):
+        """
+        Should fail: no username (empty part before "@" character).
+        """
+        email_address = "@domain.com"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+
+class EmailValidatorMiddleDomains(unittest.TestCase):
+    def test_middle_domains(self):
+        """
+        Should fail: the part between "@" and "." must be at
+        least 1 character long.
+        """
+        email_address = "user@.com"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+class EmailValidatorTopLevelDomain(unittest.TestCase):
+    """
+    The part after the last "." must be between 1 and 4 characters long,
+    containing only letters and/or digits.
+    """
+    def test_top_level_domain_too_long(self):
+        """
+        The top-level domain name is longer than 4 characters.
+        """
+        email_address = "user@domain1.com.topleveldomainname"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+    def test_top_level_invalid_characters(self):
+        """
+        The top-level domain name contains illegal characters.
+        """
+        email_address = "user@domain1.domain2.$;a"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+    def test_numbers_only(self):
+        """
+        Should fail: the top-level domain will never consist of only numbers.
+        """
+        email_address = "user@domain1.123"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+    def test_no_top_level_domain(self):
+        """
+        Should fail: no top-level domain.
+        """
+        email_address = "user@domain1"
+        self.assertRaises(InvalidEmailError,
+                          lambda: validate_email(email_address))
+
+    def test_valid_top_level_domain_multiple_domains(self):
+        """
+        Should success: valid top-level domain name with multiple domains.
+        """
+        email_address = "user@domain.org.pl"
+        self.assertTrue(validate_email(email_address))
 
 
 if __name__ == '__main__':
--- utils/email_validator.py
+++ utils/email_validator.py
@@ -1,2 +1,12 @@
+import re
+
+from utils.exceptions import InvalidEmailError
+
+email_regex = (r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.'
+               r'(?=.*?[A-Z|a-z])[A-Z|a-z|0-9]{2,4}\b')
+
+
 def validate_email(email_address):
-    return None
+    if re.fullmatch(email_regex, email_address):
+        return True
+    raise InvalidEmailError
