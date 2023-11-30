04981861405fde110eb4633d96d10c155453e838
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-11-30 17:10:58 +0100

    Added validator for telephone number.

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,12 +1,13 @@
 import unittest
-from utils.validators import validate_email
-from utils.exceptions import InvalidEmailError
+from utils.validators import validate_email, validate_telephone_number
+from utils.exceptions import InvalidEmailError, InvalidPhoneNumberError
 
 
 class EmailValidatorAtCharacterTestCase(unittest.TestCase):
     """
     Email must contain only one "@" symbol.
     """
+
     def test_single_at(self):
         """
         Single '@' in a valid email address: should return True.
@@ -47,6 +48,7 @@ class EmailValidatorUsernameTestCase(unittest.TestCase):
     """
     The part before "@" (email username) must be at least 1 character long.
     """
+
     def test_single_character(self):
         """
         Should success: single character for an email username.
@@ -88,6 +90,7 @@ class EmailValidatorTopLevelDomain(unittest.TestCase):
     The part after the last "." must be between 1 and 4 characters long,
     containing only letters and/or digits.
     """
+
     def test_top_level_domain_too_long(self):
         """
         The top-level domain name is longer than 4 characters.
@@ -126,6 +129,31 @@ class EmailValidatorTopLevelDomain(unittest.TestCase):
         """
         email_address = "user@domain.org.pl"
         self.assertTrue(validate_email(email_address))
+
+
+class ValidateTelephoneNumberTestCase(unittest.TestCase):
+    def test_raising_error_on_invalid_phone(self):
+        """
+        Should raise InvalidPhoneNumberError when invalid phone number is
+        passed.
+        """
+        invalid_phone_num = "939387393837494765454767"
+        self.assertRaises(
+            InvalidPhoneNumberError,
+            lambda: validate_telephone_number(invalid_phone_num))
+
+    def test_raising_error_on_no_phone_empty_str(self):
+        """
+        Should raise InvalidPhoneNumberError when empty string is passed
+        as an argument.
+        """
+        self.assertRaises(
+            InvalidPhoneNumberError,
+            lambda: validate_telephone_number(""))
+
+    def test_successful_validation(self):
+        phone_num = "123456789"
+        self.assertTrue(validate_telephone_number(phone_num))
 
 
 if __name__ == '__main__':
--- utils/validators.py
+++ utils/validators.py
@@ -1,12 +1,20 @@
 import re
 
-from utils.exceptions import InvalidEmailError
+from utils.exceptions import InvalidEmailError, InvalidPhoneNumberError
 
-email_regex = (r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.'
-               r'(?=.*?[A-Z|a-z])[A-Z|a-z|0-9]{2,4}\b')
+email_regex = (r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\."
+               r"(?=.*?[A-Z|a-z])[A-Z|a-z|0-9]{2,4}\b")
+
+telephone_num_regex = r"\b\d{9}\b"
 
 
 def validate_email(email_address):
     if re.fullmatch(email_regex, email_address):
         return True
     raise InvalidEmailError
+
+
+def validate_telephone_number(telephone_num):
+    if re.fullmatch(telephone_num_regex, telephone_num):
+        return True
+    raise InvalidPhoneNumberError
