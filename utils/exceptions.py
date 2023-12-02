a56344655269d2115026910f6b39659b885fc111
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-12-02 15:15:13 +0100

    Added exceptions for authorization and authentication errors.

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -12,3 +12,26 @@ class InvalidPhoneNumberError(InvalidInputError):
 
 class RoleNotFoundError(Exception):
     pass
+
+
+class InvalidCredentialsError(Exception):
+    """
+    Base class for authorization/authentication errors.
+    """
+    pass
+
+
+class InvalidAuthenticationError(InvalidCredentialsError):
+    """
+    Exception to be raised when no/invalid credentials (login or password)
+    are provided.
+    """
+    pass
+
+
+class InvalidAuthorizationError(InvalidCredentialsError):
+    """
+    Exception to be raised when the user has insufficient authorization
+    to perform an action (for instance, action requires admin credentials).
+    """
+    pass
