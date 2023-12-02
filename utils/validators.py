96d1b37cce681cbd8c0babed66dd3420cb57e950
Radosław Kuzyk <re.kuzyk@gmail.com>
2023-12-02 22:22:53 +0100

    Updated email validator in validators.py: previously the last part (after the last '.') of valid emails had to be at least 2 characters long and contain at least one letter.

--- utils/validators.py
+++ utils/validators.py
@@ -3,7 +3,7 @@ import re
 from utils.exceptions import InvalidEmailError, InvalidPhoneNumberError
 
 email_regex = (r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\."
-               r"(?=.*?[A-Z|a-z])[A-Z|a-z|0-9]{2,4}\b")
+               r"[A-Z|a-z|0-9]{1,4}\b")
 
 telephone_num_regex = r"\b\d{9}\b"
 
